b19dc3cd9bb2a0d9d16dd8525348f07310316458
PalkAlex23 <alexpytmusic@gmail.com>
2024-09-30 16:05:48 +0200

    uj JOP nezet

--- src/main/KincsesLada.java
+++ src/main/KincsesLada.java
@@ -1,18 +1,25 @@
 package main;
 
 import modell.JatekModell;
+import nezet.FelugroNezet;
+import vezerlo.FelugroVezerlo;
+/*
 import nezet.KonzolNezet;
 import vezerlo.KonzolVezerlo;
-
+*/
 
 public class KincsesLada {
 
     public static void main(String[] args) {
         // new JatekModell();
-        JatekModell modell = new JatekModell();
-        KonzolNezet nezet = new KonzolNezet();
         
-        new KonzolVezerlo(modell, nezet);
+        
+        JatekModell modell = new JatekModell();
+        // KonzolNezet nezet = new KonzolNezet();
+        FelugroNezet nezet = new FelugroNezet();
+        
+        // new KonzolVezerlo(modell, nezet);
+        new FelugroVezerlo(modell, nezet);
     }
     
 }
